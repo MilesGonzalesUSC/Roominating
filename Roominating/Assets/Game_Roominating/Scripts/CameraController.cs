257bd8b43dd155081bfd8dfba69a3121d9c701ce
Miles Gonzales <milesgon@usc.edu>
2024-09-26 13:16:39 -0700

    Fixed Camera Controller

--- Roominating/Assets/Game_Roominating/Scripts/CameraController.cs
+++ Roominating/Assets/Game_Roominating/Scripts/CameraController.cs
@@ -3,26 +3,47 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 	Vector3 inputDir = new Vector3(0, 0, 0);
+	public float MaxX = 20f;
+	public float MaxY = 20f;
+
+	public bool CanMoveCam;
 
 	public int edgeScrollSize = 20;
+
+	private void Start( )
+	{
+		CanMoveCam = true;
+	}
 	private void Update( )
 	{
+		if(CanMoveCam)
+		{
 
-		if(Input.mousePosition.x < edgeScrollSize){
-			inputDir.x = -.25f;
-		}
-		if(Input.mousePosition.y < edgeScrollSize){
-			inputDir.y = -.25f;
-		}
-		if(Input.mousePosition.x > Screen.width - edgeScrollSize) { 
-			inputDir.x = +.25f; 
-		}
-		if(Input.mousePosition.y > Screen.height - edgeScrollSize){
-			inputDir.y = +.25f;
-		}
+			if(Input.mousePosition.x < edgeScrollSize && transform.position.x >= -MaxX)
+			{
+				inputDir.x = -.25f;
+			} else
+			{
+				inputDir.x = 0f;
+			}
+			if(Input.mousePosition.y < edgeScrollSize && transform.position.y >= -MaxY)
+			{
+				inputDir.y = -.25f;
+			} else
+			{
+				inputDir.y = 0f;
 
+			}
+			if(Input.mousePosition.x > Screen.width - edgeScrollSize && transform.position.x <= MaxX)
+			{
+				inputDir.x = +.25f;
+			}
 
-
+			if(Input.mousePosition.y > Screen.height - edgeScrollSize && transform.position.y <= MaxY)
+			{
+				inputDir.y = +.25f;
+			}
+		}
 
 
 		float moveSpeed = 10f;
