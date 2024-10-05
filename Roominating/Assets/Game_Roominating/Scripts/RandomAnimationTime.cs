3250b246cc26149ff1e6154ca7522be5b00a5212
Miles Gonzales <milesgon@usc.edu>
2024-10-05 14:31:20 -0700

    Earthquake and No Fun Done

--- Roominating/Assets/Game_Roominating/Scripts/RandomAnimationTime.cs
+++ Roominating/Assets/Game_Roominating/Scripts/RandomAnimationTime.cs
@@ -7,13 +7,14 @@ public class RandomAnimationTime : MonoBehaviour
     Animator animator;
     float randomOffset;
     int layer;
+    public string State;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         randomOffset = Random.Range(0f, 1f);
         layer = animator.gameObject.layer;
-        animator.Play("Bob",layer,randomOffset);
+        animator.Play(State,layer,randomOffset);
     }
 
     // Update is called once per frame
